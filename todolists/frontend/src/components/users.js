8083f2cdbf5051d63a136d3f7ca260491afcc4fc
Ex1le <3x1le@rambler.ru>
2023-02-11 21:54:31 +0300

    react added

---
+++ todolists/frontend/src/components/users.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+
+const UserItem = ({ user }) => {
+
+    return (
+        <tr>
+            <td>
+                {user.first_name}
+            </td>
+            <td>
+                {user.last_name}
+            </td>
+        </tr>
+    )
+}
+
+const UserList = ({ users }) => {
+
+    return (
+        <table>
+            <th>
+                First name
+            </th>
+            <th>
+                Last name
+            </th>
+            {users.map((user) => <UserItem user={user} />)}
+        </table>
+    )
+}
+
+export default UserList;
--- todolists/users/views.py
+++ todolists/users/views.py
@@ -1,7 +1,6 @@
 from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
 
-
 from .models import Users
 from .serializers import UsersModelSerializer
 
